77b78066c3307efb520c3820dc610d0aa9082ad4
David Chaves <dchavesd@gmail.com>
2017-10-29 13:14:51 +0100

    Added title screen with a lot of flow. Related to issue #3. Synchronization in aliens solved.

--- title.gd
+++ title.gd
@@ -10,6 +10,8 @@ var index = 0
 func all_aliens_do(move, flip):
 	for alienname in aliennames:
 		var alien = get_node(alienname)
+		alien.get_node("Sprite").stop()
+		alien.get_node("Sprite").set_animation("duck")
 		alien.get_node("Sprite").set_animation(move)
 		alien.get_node("Sprite").set_flip_h(flip)
 		alien.get_node("Sprite").play()
@@ -20,7 +22,6 @@ func _ready():
 	for alienname in aliennames:
 		var alien = get_node(alienname)
 		alien.set_script(null)
-		alien.get_node("Sprite").stop()
 	all_aliens_do("stand",false)
 	set_fixed_process(true)
 	set_process_input(true)
