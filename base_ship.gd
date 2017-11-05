cf69e5090a6f9c2b37fd65a51e6b18bd2230b60d
hmightypirate <hcerezoc@gmail.com>
2017-11-05 16:39:35 +0100

    Added condition for the detection of the aliens in the ship
    Added a group for each alien type (blue_alien, red_alien, etc)
    
    Changed shape collision in aliens to PolligonShape (instead of Capsule)

--- base_ship.gd
+++ base_ship.gd
@@ -16,29 +16,44 @@ func _ready():
 	left_area.connect("body_exit", self, "_on_leftArea2D_body_exit")
 	right_area.connect("body_enter", self, "_on_rightArea2D_body_enter")
 	right_area.connect("body_exit", self, "_on_rightArea2D_body_exit")
-	
-	print ("IS MONITOR ", left_area.is_monitoring_enabled())
-	
-	set_fixed_process(true)
+
+	set_fixed_process(false)
 	
 func _fixed_process(delta):
 	print ("LEFTI ", left_area.get_overlapping_bodies())
 	
 func _on_leftArea2D_body_enter( body ):
 	print ("BODY ", body)
+	print ("body parent ", body.get_parent())
+	print ("FLUX ", body.get_name())
+	print ("FLUXI ", body)
+	print ("BODYNAME ", body.get_type())
+	
+	if body.is_in_group(my_alien):
+		print ("Good name ") 
+		# Check if it overlaps with the right area
+		if right_area.overlaps_body(body):
+			print ("Do the magic here ")
+
+	
 	pass # replace with function body
 
 
 func _on_leftArea2D_body_exit( body ):
-	print ("BODY ", body)
 	pass # replace with function body
 
 
 func _on_rightArea2D_body_enter( body ):
 	print ("BODY ", body)
+	
+	if body.is_in_group(my_alien):
+		print ("gOOD NAME ")
+		# Check if it overlaps with the left area
+		if left_area.overlaps_body(body):
+			print ("Do the magic here ")
+	
 	pass # replace with function body
 
 func _on_rightArea2D_body_exit( body ):
-	print ("BODY ", body)
 	pass # replace with function body
 
